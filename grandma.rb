b0f498f33cce9ddf7a526b190850de8ac55c391a
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-09 00:25:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   end
-  if phrase != phrase.upcase()
+  if phrase == phrase.downcase()
     return "HUH?! SPEAK UP, SONNY!"
-  else
+  else phrase==phrase.upcase()
     return "NO, NOT SINCE 1938!"
   end
 end
